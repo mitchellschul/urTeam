37b348f2ef285021ccbe9da7341682132308c8a4
Mitchell Schuldinger <mitchellasdf@gmail.com>
2020-12-15 01:36:57 -0500

    Initial Commit

---
+++ urTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  urTeam
+//
+//  Created by Mitchell Schuldinger on 12/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
